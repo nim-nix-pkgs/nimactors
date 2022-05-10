7d348517144707dc2c8c8917df0c301670e1b49d
hugosenari <hugosenari@gmail.com>
2022-05-09 21:07:15 -0300

    feat(nimpkgs): update lock

--- flake.nix
+++ flake.nix
@@ -7,22 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."nimactors-master".dir   = "master";
-  inputs."nimactors-master".owner = "nim-nix-pkgs";
-  inputs."nimactors-master".ref   = "master";
-  inputs."nimactors-master".repo  = "nimactors";
-  inputs."nimactors-master".type  = "github";
-  inputs."nimactors-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."nimactors-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."nimactors-v0_1_0".dir   = "v0_1_0";
-  inputs."nimactors-v0_1_0".owner = "nim-nix-pkgs";
-  inputs."nimactors-v0_1_0".ref   = "master";
-  inputs."nimactors-v0_1_0".repo  = "nimactors";
-  inputs."nimactors-v0_1_0".type  = "github";
-  inputs."nimactors-v0_1_0".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."nimactors-v0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
